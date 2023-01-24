e4f876b7d95717a329d2acc9dc28da9230fc2009
andrxw66 <andreyshihalev099@gmail.com>
2023-01-24 12:43:59 +0300

    add some styles on links

--- app/components/Header/HeaderLink/HeaderLink.tsx
+++ app/components/Header/HeaderLink/HeaderLink.tsx
@@ -1,10 +1,11 @@
 import { Paragraph } from '@/app/ui'
 import React from 'react'
 import { HeaderLinkProps } from './HeaderLink.props'
+import styles from './HeaderLink.module.scss'
 
 export const HeaderLink = ({ name }: HeaderLinkProps) => {
 	return (
-		<a href="#">
+		<a href="#" className={styles.link}>
 			<Paragraph>{name}</Paragraph>
 		</a>
 	)
